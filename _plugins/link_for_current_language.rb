b61bf148a0ac2ac67c9200190ae1924e4bc3301d
Renato "Lond" Cerqueira <renato@lond.com.br>
2025-09-19 17:05:46 +0200

    Show proper links on the navigation bar based on locale

---
+++ _plugins/link_for_current_language.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class LinkForCurrentLanguage < Liquid::Tag
+  #======================================
+  # initialize
+  #======================================
+  def initialize(tag_name, key, tokens)
+    super
+    @key = key.strip
+  end
+
+  #======================================
+  # render
+  #======================================
+  def render(context)
+    key = if context[@key].to_s == ""
+      @key
+    else # Check for page variable
+      context[@key].to_s
+    end
+
+    key = Liquid::Template.parse(key).render(context) # Parses and renders some Liquid syntax on arguments (allows expansions)
+
+    site = context.registers[:site]
+    multi_language_pages = site.pages.filter { |page| !page.data["page_id"].nil? }
+    multi_language_pages.each do |page|
+      next unless page["redirect_from"]
+      return page["permalink"] if page["redirect_from"].include? key
+    end
+
+    key
+  end
+end
+
+Liquid::Template.register_tag("link_for_current_language", LinkForCurrentLanguage)
